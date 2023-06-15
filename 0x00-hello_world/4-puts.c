a5fba27dfa8167d8e888a16d92372732ac7fbe1f
Samya <samyael@gmail.com>
2023-06-15 12:46:11 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-/**
- * main - 4-puts.c
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
-}
